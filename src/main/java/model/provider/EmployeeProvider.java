3604c7d61f6d30cc57d300fe2922be34f734e576
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2020-11-11 21:22:08 -0500

    EmployeeProvider started

--- src/main/java/model/provider/EmployeeProvider.java
+++ src/main/java/model/provider/EmployeeProvider.java
@@ -1,4 +1,18 @@
 package model.provider;
 
+import model.dto.EmployeeDTO;
+import db.MySQLConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class EmployeeProvider {
+
+    public EmployeeDTO getEmployeeById(int id) {
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        MySQLConnection connection = new MySQLConnection();
+        return null;
+    }
+
 }
