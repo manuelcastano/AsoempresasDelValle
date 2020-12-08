09b26b72e3affe018e368855d8fa6970724bec35
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2020-12-08 11:56:03 -0500

    Front integrated to proyect

--- src/main/Vista Proyecto/js/login.js
+++
@@ -1,3 +0,0 @@
-const user = document.getElementById('user');
-const password = document.getElementById('passW');
-const logBtn = document.getElementById('logBtn');
---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,10 @@
+const user = document.getElementById('user');
+const password = document.getElementById('passW');
+const logBtn = document.getElementById('logBtn');
+
+login = ()=>{
+
+    
+}
+
+logBtn.addEventListener('click', login);
