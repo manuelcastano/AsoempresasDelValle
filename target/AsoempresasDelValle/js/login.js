90791fdcb8ce411048d073cd8b9b05aa1234488c
manuelcastano <manuelsabe186@gmail.com>
2020-12-09 18:41:28 -0500

    making the graphics

--- target/AsoempresasDelValle/js/login.js
+++ target/AsoempresasDelValle/js/login.js
@@ -2,11 +2,12 @@ const user = document.getElementById('user');
 const password = document.getElementById('passW');
 const logBtn = document.getElementById('logBtn');
 
-log = ()=>{
+log = () => {
     
     let paso = true;
     if(paso)
         window.location.href = 'viewCompanies.html';
+        
     else
         alert();
 
