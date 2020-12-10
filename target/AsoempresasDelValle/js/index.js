a112c26e3041aadb31882b01b725e0352ef8cfcd
manuelcastano <manuelsabe186@gmail.com>
2020-12-10 10:22:45 -0500

    fixing conflicts

---
+++ target/AsoempresasDelValle/js/index.js
@@ -0,0 +1,10 @@
+const ingBtn = document.getElementById('ingBtn');
+const carrusel = document.getElementById('carrusel');
+
+enter = () => {
+    window.location.href = 'login.html';
+    
+}
+
+
+ingBtn.addEventListener('click', enter);
