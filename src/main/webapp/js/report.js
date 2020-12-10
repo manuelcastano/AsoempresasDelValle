88dbd63e9309f4ef4d8d2b80b6a6a956087dc1bf
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2020-12-10 08:14:02 -0500

    report

---
+++ src/main/webapp/js/report.js
@@ -0,0 +1,11 @@
+const selection = document.getElementById('seleccion');
+const fFinal = document.getElementById('FechaInicial');
+const fInicial = document.getElementById('FechaFinal');
+const generarBtn = document.getElementById('generarBtn');
+
+generar = () =>{
+
+
+}
+
+generarBtn.addEventListener('click', generar);
