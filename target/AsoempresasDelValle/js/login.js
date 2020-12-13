b68bf1842885ecf08a3736b199af7db7e84fe11b
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2020-12-13 09:50:30 -0500

    Login with website checked

--- target/AsoempresasDelValle/js/login.js
+++ target/AsoempresasDelValle/js/login.js
@@ -6,23 +6,23 @@ const log = () => {
 
     let userObj = {
         id:0,
-        user: this.user.value,
-        password: this.passW.value
+        user: user.value,
+        password: passW.value
     }
     console.log(JSON.stringify(userObj));
-    let paso = false;
-
     let xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange', ()=>{
-        if(xhr.readyState===4)
-            paso = true;
+        if(xhr.readyState===4){
+            let response = JSON.stringify(xhr.responseText);
+            console.log(response);
+            if(response.includes('exitosa')){
+                window.location.href = 'viewCompanies.html';
+            }else{
+                alert('Usuario o contraseña incorrecta');
+            }
+            
+        }
     });
-
-    if(paso)
-        window.location.href = 'viewCompanies.html';
-    else
-        alert();
-
     xhr.open('POST', 'http://localhost:8080/AsoempresasDelValle/api/employee/login');
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(userObj));
