e0cd63502e54cc2ae4edf34ff37519369edab954
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2020-12-13 11:38:36 -0500

    Minimun changes to report.js

--- src/main/webapp/js/report.js
+++ src/main/webapp/js/report.js
@@ -2,10 +2,14 @@ const selection = document.getElementById('seleccion');
 const fFinal = document.getElementById('FechaInicial');
 const fInicial = document.getElementById('FechaFinal');
 const generarBtn = document.getElementById('generarBtn');
+const company = JSON.parse(window.localStorage.getItem('<<Poner aqui el nombre de la variable del local storage>>')  );
+
+
 
 generar = () =>{
 
 
 }
+
 
 generarBtn.addEventListener('click', generar);
