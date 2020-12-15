171ecc85bbe820c6a2898679d03b5e1eeda24df4
Santiago Figueroa Aguirre <santiago.figueroa1@correo.icesi.edu.co>
2020-12-14 19:58:29 -0500

    Merge between develop and master

--- target/AsoempresasDelValle/js/viewCompanies.js
+++ target/AsoempresasDelValle/js/viewCompanies.js
@@ -1,19 +1,20 @@
-const companiesContainer = document.getElementById('companiesContainer')
+const companiesContainer = document.getElementById('companiesContainer');
+
 const getAllCompanies = ()=>{
-let xhr = new XMLHttpRequest();
-xhr.addEventListener('readystatechange', ()=>{
-    if(xhr.readyState === 4){
-        let json = xhr.responseText;
-        let response = JSON.parse(json);
-        console.log(response);
-        for(let i=0; i<response.length; i++){
-            let companyDTO = response[i];
-            let view = new CompanyView(companyDTO);
-            companiesContainer.appendChild(view.render());
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', ()=>{
+        if(xhr.readyState === 4){
+            let json = xhr.responseText;
+            let response = JSON.parse(json);
+            console.log(response);
+            for(let i=0; i<response.length; i++){
+                let companyDTO = response[i];
+                let view = new CompanyView(companyDTO);
+                companiesContainer.appendChild(view.render());
+            }
         }
-    }
-});
-    xhr.open('GET','http://localhost:8080/AsoempresasDelValle/api/company/getallcompanies');
+    });
+    xhr.open('GET', 'http://localhost:8080/AsoempresasDelValle/api/company/getallcompanies');
     xhr.send();
 };
 
